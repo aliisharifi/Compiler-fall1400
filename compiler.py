81d6b329d172a7c1030936c3d99a3fe50f2cc4dd
ali sharifi <a.sharifi1379@gmail.com>
2021-10-15 18:03:18 +0330

    table completed

--- compiler.py
+++ compiler.py
@@ -34,6 +34,79 @@ after_id_acc += whitespace
 """
 table = {}
 ################
+# defining sets
+digit = [i for i in '0123456789']
+lowercase_letters = {chr(i) for i in range(ord('a'), ord('z') + 1)}
+uppercase_letters = {i.upper() for i in lowercase_letters}
+letter = lowercase_letters + uppercase_letters
+after_id_acc = {' ', '=', '<'} # TODO complete
+after_num_acc = {} # TODO complete
+symbol_except_eq = {i for i in ';:,[]()+-*<'}
+whitespace = {i for i in ' \n\r\t\v\f'}
+alphabet = {chr(i) for i in range(32, 127)} + whitespace + ''  # all printable ASCIIs
+
+#TODO defining others!
+start_other = {}
+ID_KEY_other = {}
+NUM_other = {}
+
+
+#initializing table
+table.update('start') = {}
+for i in letter:
+    table['start'].update(i) = 'ID_KEY'
+for i in digit:
+    table['start'].update(i) = 'NUM'
+for i in symbol_except_eq:
+    table['start'].update(i) = 'symbol_1'
+table['start'].update('=') = 'symbol2'
+table['start'].update('/') = 'COM_1'
+for i in whitespace:
+    table['start'].update(i) = 'whitespace'
+table['start'].update('*') ='v' #TODO Check V?
+
+for i in letter + digit:
+    table['ID_KEY'].update(i) = 'ID_KEY'
+for i in after_id_acc:
+    table['ID_KEY'].update(i) = 'ID_KEY_ACC_*'
+for i in ID_KEY_other:
+    table['ID_KEY'].update(i) = 'invalid_input'
+
+for i in digit:
+    table['NUM'].update(i) = 'NUM'
+for i in after_num_acc:
+    table['NUM'].update(i) = 'NUM_ACC_*'
+for i in NUM_other:
+    table['NUM'].update(i) = 'invalid_number'
+
+table['symbol_2'].update('=') = 'symbol_3'
+for i in alphabet - {'='}:
+    table['symbol_2'].update(i) = 'symbol_4_*'
+
+for i in alphabet - {'/', '*'}:
+    table['COM_1'].update(i) = 'invalid_input_*'
+table['COM_1'].update('*') = 'COM_2'
+table['COM_1'].update('/') = 'COM_4'
+
+for i in alphabet - {'', '*'}:
+    table['COM_2'].update(i) = 'COM_2'
+table['COM_2'].update('') = 'unclosed_comment'
+table['COM_2'].update('*') = 'COM_3'
+
+for i in alphabet - {'', '\n'}:
+    table['COM_4'].update('i') = 'COM_4'
+table['COM_4'].update('') = 'COM_2_ACC'
+table['COM_4'].update('\n') = 'COM_2_ACC'
+
+for i in alphabet - {'', '/'}:
+    table['COM_3'].update(i) = 'COM_2'
+table['COM_3'].update('') = 'unclosed_comment'
+table['COM_3'].update('/') = 'COM_2_ACC'
+
+for i in alphabet - {'/'}:
+    table['V'].update(i) = 'invalid_input_*'
+table['/'].update('/') = 'unmatched_comment'
+#################
 
 lineCount = 1
 state = ""
