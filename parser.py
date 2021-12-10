5a029efe67b7c7587e681511bd0ba22060f61878
Ali Sharifi <a.sharifi1379@gmail.com>
2021-12-10 11:54:50 +0330

    edge type

--- parser.py
+++ parser.py
@@ -10,26 +10,26 @@ la_role, la_tok = get_next_token()
 # node with id 0?
 traverse_list = []
 f.write('Program\n')
-branch_edges = '|--- '
-cont_edges = '|    '
+middle_edge = '├── '
+last_edge = '└── '
+cont_edges = '│   '
 space = '    '
 
-def write_nonterm(node_, res_):
-    f.write(calc_line_first_part() + branch_edges + res_.name + '\n')
+def write_nonterm(node_, res_, edge_type_):
+    # f.write(calc_line_first_part() + branch_edges + res_.name + '\n')
+    f.write(calc_line_first_part() + edge_type_ + res_.name + '\n')
     traverse_list.append(node_)
     traverse_list.append(res_)
 
-def write_term(node_, la_role_, la_tok_):
-    f.write(calc_line_first_part() + branch_edges + '(' + la_role_ + ', ' + la_tok_ + ')' + '\n')
+def write_term(node_, la_role_, la_tok_, edge_type_):
+    # f.write(calc_line_first_part() + branch_edges + '(' + la_role_ + ', ' + la_tok_ + ')' + '\n')
+    f.write(calc_line_first_part() + edge_type_ + '(' + la_role_ + ', ' + la_tok_ + ')' + '\n')
     traverse_list.append(node_)
 
-def write_epsilon(node_):
-    f.write(calc_line_first_part() + branch_edges + 'EPSILON' + '\n')
+def write_epsilon(node_, edge_type_):
+    # f.write(calc_line_first_part() + branch_edges + 'EPSILON' + '\n')
+    f.write(calc_line_first_part() + edge_type_ + 'EPSILON' + '\n')
     traverse_list.append(node_)
-
-# def get_node_nonterm:
-#     """returns the nonterminal of the diagram that node belongs to"""
-#     pass
 
 def calc_line_first_part():
     line_first_part = ''
@@ -49,59 +49,57 @@ while traverse_list:
             if la_role == 'NUM' or la_role == 'ID':
                 if la_role in last_node.final.follow:
                     break
-                # if la_role not in last_node.final.follow:
                 else:
                     # illegal
                     la_role, la_tok = get_next_token()
             else:
                 if la_tok in last_node.final.follow:
                     break
-                # if la_tok not in last_node.final.follow:
                 else:
                     # illegal
                     la_role, la_tok = get_next_token()
         continue
     for term_nonterm, node in last_node.to.items():
         res = getNonTermByName(term_nonterm)
+        edge_type = last_edge if node.final else middle_edge
         if res:
             if la_role == 'NUM' or la_role == 'ID':
                 if la_role in res.first:
-                    write_nonterm(node, res)
+                    write_nonterm(node, res, edge_type)
                     match = True
                     break
                 elif 'EPSILON' in res.first and la_role in res.follow:
-                    write_nonterm(node, res)
+                    write_nonterm(node, res, edge_type)
                     match = True
                     break
             else:
                 if la_tok in res.first:
-                    write_nonterm(node, res)
+                    write_nonterm(node, res, edge_type)
                     match = True
                     break
                 elif 'EPSILON' in res.first and la_tok in res.follow:
-                    write_nonterm(node, res)
+                    write_nonterm(node, res, edge_type)
                     match = True
                     break 
         else:
             if la_role == 'NUM' or la_role == 'ID':
                 if la_role == term_nonterm:
-                    write_term(node, la_role, la_tok)
+                    write_term(node, la_role, la_tok, edge_type)
                     match = True
                     break
                 elif term_nonterm == 'EPSILON' and la_role in node.final.follow:
-                    write_epsilon(node)
+                    write_epsilon(node, edge_type)
                     match = True
                     break
             else:
                 if la_tok == term_nonterm:
-                    write_term(node, la_role, la_tok)
+                    write_term(node, la_role, la_tok, edge_type)
                     match = True
                     break
                 elif term_nonterm == 'EPSILON' and la_tok in node.final.follow:
-                    write_epsilon(node)
+                    write_epsilon(node, edge_type)
                     match = True
                     break
-    # la_role, la_tok = get_next_token()
     if match:
         la_role, la_tok = get_next_token()
         match = False
